2db121414439b387fc27fa0a1a5396478e3614c0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-22 10:07:32 +0700

    Membuat options vs composition

---
+++ src/components/CompositionComponent.vue
@@ -0,0 +1,17 @@
+<!-- composition API -->
+<script setup>
+import MainButton from './MainButton.vue';
+
+    const name = 'ini Composition Component API'
+
+    const showName = () => console.log(name)
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+    <!-- @click = event handler -->
+    <MainButton @click="showName" />
+</template>
+
+<!-- gak perlu dimasukan juga tidak apa-apa -->
+<style></style>
---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,25 @@
+<!-- option API -->
+<script>
+import MainButton from './MainButton.vue';
+
+export default {
+    data() {
+        return {
+            name: 'ini Option Component API' 
+        }
+    },
+    methods: {
+        showName() {
+            console.log(this.name)
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+    <button @click="showName">Show Name</button>
+</template>
+
+<!-- gak perlu dimasukan juga tidak apa-apa -->
+<style></style>
