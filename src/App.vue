2d985bfadd8d2faae22f5702b19fdda8c3ff3f48
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-22 17:02:32 +0700

    Membuat list Rendering

--- src/App.vue
+++ src/App.vue
@@ -34,13 +34,13 @@ const user = reactive({name: 'Fadilah', age:'23'})
 const orang = reactive(['Nmax', 49303, 'Bandung', 'Pasundan'])
 
 // computed property
-const author = reactive({
-  name: 'JK Rowling',
-  books: [
-    'Harry Poter 1',
-    'Harry Poter 2',
-  ]
-})
+// const author = reactive({
+//   name: 'JK Rowling',
+//   books: [
+//     'Harry Poter 1',
+//     'Harry Poter 2',
+//   ]
+// })
 
 const count3 = ref(0)
 const increment2 = () => {
@@ -61,6 +61,26 @@ const addBook = () => {
 // end--------------------------
 // conditional redering
 const showAuthor = ref(true) // authornya tampil jika bernilai true
+// end--------------------------
+
+// list rendering
+const author = reactive({
+  name: 'JK Rowling',
+  books: [
+    {
+      title: 'Harry Poter 1',
+      releaseDate: '1999',
+      reviews: [5,5,5],
+      avgReview: 5
+    },
+    {
+      title: 'Harry Poter 2',
+      releaseDate: '2001',
+      reviews: [1,2,3],
+      avgReview: 2
+    },
+  ]
+})
 // end--------------------------
 
 </script>
@@ -104,6 +124,45 @@ const showAuthor = ref(true) // authornya tampil jika bernilai true
     <span v-else-if="author.books.length === 1">Have A Book</span>
     <span v-else>Don't Have a book</span>
   </h2>
+  <p>------------------------------</p>
+
+  <p>List Rendering</p>
+  <h2>Books:</h2>
+  <ul>
+    <!-- mapping -->
+    <li v-for="book in author.books" :key="book.title">
+       {{ book.releaseDate }}
+    </li>
+    <li v-for="(book, index) in author.books" :key="`${index}`">
+       {{ book.title }}
+    </li>
+  
+    <li v-for="({title, releaseDate}, index) in author.books" :key="`book-${index}`">
+       {{ title }} - {{ releaseDate }}
+       <span v-for="(review, reviewIndex) in reviews" :key="`review-${reviewIndex}`">
+        {{ review }}
+       </span>
+    </li>
+  </ul>
+
+  <ul>
+    <li v-for="(data, key, index) in author" :key="key">
+      {{ index }}. {{ key }}: {{ data }}
+    </li>
+  </ul>
+
+  <ol>
+    <li v-for="(item, index) in 10" :key="index">
+     {{ item }} Nabil Fadilah
+    </li>
+  </ol>
+
+  <ul>
+    <template v-for="(item, index) in author.books" :key="index">
+      <li v-if="item.avgReview > 3">{{ item.title }}</li>
+      <MainButton v-if="item.avgReview >3" :title="item.title" />
+    </template>
+  </ul>
   <p>------------------------------</p>
   
   <header>
